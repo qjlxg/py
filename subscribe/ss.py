584512578490671d76e7feb0583bde1b80feed0a
qjlxg <qjlxg@163.com>
2025-04-16 23:09:33 +0800

    Update ss.py

--- subscribe/ss.py
+++ subscribe/ss.py
@@ -22,7 +22,7 @@ def is_valid_url(url):
         return False
 
 # 获取原始URL列表
-sub_all_clash_url = 'https://raw.githubusercontent.com/qjlxg/aggregator/refs/heads/main/tools/ss-url'
+sub_all_clash_url = 'https://raw.githubusercontent.com/qjlxg/py/refs/heads/main/tools/ss-url?token=GHSAT0AAAAAADBYZWPHEHO3GXJT5MZ6AADEZ77ZBIA'
 try:
     response = requests.get(sub_all_clash_url, headers=headers, timeout=256)
     response.raise_for_status()
