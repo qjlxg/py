3a52fd5200dad6befe14927001bf5190347b488f
qjlxg <qjlxg@163.com>
2025-04-16 16:22:50 +0800

    Update convert_to_base64.py

--- subscribe/convert_to_base64.py
+++ subscribe/convert_to_base64.py
@@ -36,12 +36,12 @@ def convert_multiple_to_base64(urls):
 
 if __name__ == "__main__":
     urls = [
-        "https://raw.githubusercontent.com/dimzon/scaling-sniffle/main/all-sort.txt",
-        "https://raw.githubusercontent.com/V2RAYCONFIGSPOOL/V2RAY_SUB/refs/heads/main/V2RAY_SUB.txt",
-        "https://raw.githubusercontent.com/Space-00/V2ray-configs/refs/heads/main/config.txt",
-        "https://raw.githubusercontent.com/MrMohebi/xray-proxy-grabber-telegram/master/collected-proxies/row-url/all.txt",
-        "https://raw.githubusercontent.com/asakura42/vss/master/output.txt",
-        "https://raw.githubusercontent.com/qjlxg/ss/refs/heads/master/list_raw.txt",
-        "https://raw.githubusercontent.com/snaCW/Config/refs/heads/main/config.txt"
+        "https://raw.githubusercontent.com/qjlxg/py/refs/heads/main/trial.yaml?token=GHSAT0AAAAAADBYZWPHNVSLSSSZI2E2LYCWZ77NDTA",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
     ]
     convert_multiple_to_base64(urls)
