f9531a50cba4f614351f586629714de2b0e4c2ae
qjlxg <qjlxg@163.com>
2025-04-17 21:23:58 +0800

    Update TG_proxy_main.py

--- TG_proxy_main.py
+++ TG_proxy_main.py
@@ -372,14 +372,39 @@ def get_sub_url():
 
             
   
-
+def get_kkzui():
+    # ========== 抓取 kkzui.com 的节点 ==========
+    try:
+        headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.53"}
+        res = requests.get("https://kkzui.com/jd?orderby=modified",headers=headers)
+        article_url = re.search(r'<h2 class="item-heading"><a href="(https://kkzui.com/(.*?)\.html)"',res.text).groups()[0]
+        #print(article_url)
+        res = requests.get(article_url,headers=headers)
+        sub_url = re.search(r'<p><strong>这是v2订阅地址</strong>：(.*?)</p>',res.text).groups()[0]
+        print(sub_url)
+        e_sub.append(sub_url)
+        print("获取kkzui.com完成！")
+    except:
+        print("获取kkzui.com失败！")
+    try:
+        headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.53"}
+        res = requests.get("https://www.cfmem.com/search/label/free",headers=headers)
+        article_url = re.search(r"https?://www\.cfmem\.com/\d{4}/\d{2}/\S+v2rayclash-vpn.html",res.text).group()
+        #print(article_url)
+        res = requests.get(article_url,headers=headers)
+        sub_url = re.search(r'>v2ray订阅链接&#65306;(.*?)</span>',res.text).groups()[0]
+        print(sub_url)
+        try_sub.append(sub_url)
+        e_sub.append(sub_url)
+    except Exception as e:
         print(e)
         
     
 if __name__ == '__main__':
     print("========== 开始获取机场订阅链接 ==========")
     get_sub_url()
-   
+    print("========== 开始获取kkzui.com订阅链接 ==========")
+    get_kkzui()
     print("========== 开始获取频道订阅链接 ==========")
     for url in urls:
         #print(url, "开始获取......")
