aab5f1e60d36a9a66d727524692b1889ef6283d4
qjlxg <qjlxg@163.com>
2025-04-17 18:33:55 +0800

    Update TG_proxy_main.py

--- TG_proxy_main.py
+++ TG_proxy_main.py
@@ -32,9 +32,9 @@ else:
 #文件路径
 update_path = "./sub/"
 #所有的clash订阅链接
-end_list_clash = ['https://raw.githubusercontent.com/qjlxg/aggregator/refs/heads/main/data/clash.yaml']
+end_list_clash = ["https://raw.githubusercontent.com/qjlxg/aggregator/refs/heads/main/data/clash.yaml"]
 #所有的v2ray订阅链接
-end_list_v2ray = ['https://raw.githubusercontent.com/qjlxg/aggregator/refs/heads/main/data/all_clash.txt','https://raw.githubusercontent.com/qjlxg/aggregator/refs/heads/main/data/api.txt','https://raw.githubusercontent.com/qjlxg/aggregator/refs/heads/main/data/transporter.txt']
+end_list_v2ray = ["https://raw.githubusercontent.com/qjlxg/aggregator/refs/heads/main/data/all_clash.txt"]
 #所有的节点明文信息
 end_bas64 = []
 #获得格式化后的链接
